96b8b657e8d83528a830f7819743a1d61abed9b4
zinntikumugai <uesitananame55@gmail.com>
2016-02-20 19:26:19 +0900

    fast commit

---
+++ src/main/java/com/github/zinntikumugai/nofreeaccount/NoFreeAccount.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package com.github.zinntikumugai.nofreeaccount;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @author zinntikumugai
+ *
+ */
+public class NoFreeAccount extends Plugin {
+
+	//プラグイン読み込み
+	@Override
+	public void onEnable() {
+		getLogger().info("NoFreeAccount load!");
+	}
+}
